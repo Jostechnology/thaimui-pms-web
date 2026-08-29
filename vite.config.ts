a7e5698925e52d59bb427109f9f3d1af0b1c7588
jaoaurai <jaoaurai@gmail.com>
2026-08-29 10:05:03 +0700

    switched allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     },
     open: true,
     cors: true,
-    allowedHosts: ["test-pms.jostechnology.co.th"],
+    allowedHosts: ["app-pms.potal-thaimui.com"],
   },
   preview: {
     port: 4173,
